3534438a33f6c132e21efed79e1094bf574b48a7
Phat Hoang <lop98.hoangtanphat@gmail.com>
2021-01-23 12:54:08 +0700

    add continue, stop, menu button to PlayScene

--- FlappyBird/Assets/GameManager.cs
+++ FlappyBird/Assets/GameManager.cs
@@ -6,15 +6,20 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverCanvas;
+    public GameObject stopCanvas;
+    public GameObject scoreCanvas;
 
     private void Start()
     {
         Time.timeScale = 1;
         gameOverCanvas.SetActive(false);
+        stopCanvas.SetActive(false);
     }
 
     public void GameOver()
     {
+        // use for disable continue Button
+        scoreCanvas.GetComponentInChildren<UnityEngine.UI.Button>().interactable = false;
         gameOverCanvas.SetActive(true);
         Time.timeScale = 0;
     }
@@ -22,5 +27,23 @@ public class GameManager : MonoBehaviour
     public void Replay()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Stop()
+    {
+        Time.timeScale = 0;
+        stopCanvas.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        gameOverCanvas.SetActive(false);
+        stopCanvas.SetActive(false);
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
